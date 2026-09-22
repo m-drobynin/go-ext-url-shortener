5f1da781bb52cc9e85020aba60e1c50ca477fe75
Mikhail Drobynin <mdrobynin@yandex-team.ru>
2026-09-22 07:59:46 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"m-drobynin/go-ext-url-shortener/internal/handler"
+	"net/http"
+)
+
+const urlLength = 10
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	http.HandleFunc("/", handler.HandleShortenerRequest)
+
+	return http.ListenAndServe(`:8080`, nil)
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,69 @@
+package handler
+
+import (
+	"io"
+	"m-drobynin/go-ext-url-shortener/internal/model"
+	"m-drobynin/go-ext-url-shortener/internal/utils"
+	"net/http"
+)
+
+const urlLength = 10
+
+func handleSave(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	originalURL := string(body)
+	generatedURLCode, err := utils.RandomCode(urlLength)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	model.Put(generatedURLCode, originalURL)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("http://localhost:8080/" + generatedURLCode))
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	var urlCode = r.URL.Path
+
+	ok, res := model.Get(urlCode)
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("not found"))
+		return
+	}
+
+	w.WriteHeader(http.StatusPermanentRedirect)
+	w.Header().Add("Location", res)
+}
+
+func HandleShortenerRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		handleSave(w, r)
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		handleGet(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+	w.Write([]byte("method is not supported"))
+}
